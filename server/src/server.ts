6a7811a096c6038185a92883ec9739ef599156bb
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2022-05-06 22:25:19 -0400

    adding cors again, omg

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 
 import { NodemailerMailAdapter } from './adapters/nodemailter/nodemailer-mail-adapter'
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository'
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
