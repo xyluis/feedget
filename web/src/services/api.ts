d3919c6e05149fd16c3587176893fb3037ed17bf
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2022-05-06 22:30:36 -0400

    gambiarra sim nessa merda fodase

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-console.log(import.meta.env.VITE_API_URL)
-
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*"
+  }
 })
